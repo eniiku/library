ee556e7098407c3a93beded6efe05c27e88b0660
David Enikuomehin <davidenikuomehin1@gmail.com>
2022-06-03 04:35:05 +0100

    update script <unknown>

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: 'The Hobbit',
+    author: 'J.R.R Tolkien',
+    pages: '295 pages',
+    status: 'not reed yet',
+  },
+
+];
 
 function addBookToLibray() {
   const book = {
     title: prompt('What is the name of your book? '),
     author: `by ${ prompt('Who is the author of this book? ') }`,
     pages: prompt('How many pages does this book have? '),
-    read: prompt('Have you read this book? '),
+    status: prompt('Have you read this book? '),
   };
   myLibrary.push(book);
 }
