6f69e239c66893396548fe631180586f8861781e
tekyuma <lawrencedaviid@gmail.com>
2022-06-14 18:49:44 +0100

    lint & update script

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 const myLibrary = [
-	{
-		title: 'The Hobbit',
-		author: 'J.R.R. Thompson',
-		pages: '295 pages',
-		read: 'not read yet'
-	},
-	{
-		title: 'Ugly Love',
-		author: 'Collen Hoover',
-		pages: '314 pages',
-		read: 'read'
-	}
-]
+  {
+    title: 'The Hobbit',
+    author: 'J.R.R. Thompson',
+    pages: '295 pages',
+    read: 'not read yet',
+  },
+  {
+    title: 'Ugly Love',
+    author: 'Collen Hoover',
+    pages: '314 pages',
+    read: 'read',
+  },
+];
 
 function Book(parameters) {
-	this.title = parameters.title,
-	this.author = parameters.author,
-	this.pages = parameters.pages,
-	this.read = parameters.read
+  (this.title = parameters.title),
+    (this.author = parameters.author),
+    (this.pages = parameters.pages),
+    (this.read = parameters.read);
 }
 
 function addBookToLibrary() {
-		const bookParameters = {
-				title: prompt('Name of book?'),
-				author: prompt('Who made book? '),
-				pages: prompt('How many pages does book have? '),
-				read: prompt('Have you read this book yet')
-		}
-	
-		const book = new Book(bookParameters);
-		myLibrary.push(book);
+  const bookParameters = {
+    title: prompt('Name of book?'),
+    author: prompt('Who made book? '),
+    pages: prompt('How many pages does book have? '),
+    read: prompt('Have you read this book yet'),
+  };
+
+  const book = new Book(bookParameters);
+  myLibrary.push(book);
 }
 
 function displayBooks(library = []) {
-	const container = document.querySelector('[data-container]');
-		container.innerHTML = library.map(book => {
-				return `
+  const container = document.querySelector('[data-container]');
+  container.innerHTML = library
+    .map((book) => {
+      return `
 					<div class="card">
 						<ul>
 							<li>
@@ -52,16 +53,17 @@ function displayBooks(library = []) {
 							</li>
 						</ul>
 					</div>
-				`
-		}).join('');
+				`;
+    })
+    .join('');
 }
 
 function displayForm() {
-	const formButton = document.querySelector('[data-button=form]');
-	formButton.addEventListener('click', e => {
-		const form = document.querySelector('[data-form]');
-		form.setAttribute('style', 'display: flex;') 
-	})
+  const formButton = document.querySelector('[data-button=form]');
+  formButton.addEventListener('click', (e) => {
+    const form = document.querySelector('[data-form]');
+    form.setAttribute('style', 'display: flex;');
+  });
 }
 
 displayForm();
